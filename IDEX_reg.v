d285fa03089ff9f346005a4577157dca6927db0e
Barak-H <mobarak.abdulghani@gmail.com>
2017-10-18 14:41:31 -0700

    Revert "Add files via upload"

--- IDEX_reg.v
+++ IDEX_reg.v
@@ -1,80 +1,42 @@
 `timescale 1ns / 1ps
 
 module IDEX_reg(
-//	 	inputs
-//			For internal use:
-				Clk,
-			
-//			To Pass through:
-//				Control Lines;
-					RegWrite,
-					MemtoReg,
-					MemWrite,
-					MemRead,
-					RegDst,
-					ALUOp,
-					ALUSrc,
+	/* inputs */ Clk, ReadData1, ReadData2, immExt, rt, rd, ID_PCAddResult,
+				 RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite, /*Jump,*/ CondMov,
 
-//				Data lines;
-					ID_PCAddResult,
-					ReadData1,
-					ReadData2,
-					immExt,
-					rt,
-					rd,
-				
-                
-//		outputs
-//			To Pass through:
-//				Control Lines;
-					EX_RegWrite,
-					EX_MemtoReg,
-					EX_MemWrite,
-					EX_MemRead,
-					EX_RegDst,
-					EX_ALUOp,
-					EX_ALUSrc,
-					
-//				Data lines;
-					EX_PCAddResult,
-					EX_ReadData1,
-					EX_ReadData2,
-					EX_immExt,
-					EX_rt,
-					EX_rd );
-
+	/* outputs */ EX_ReadData1, EX_ReadData2, EX_immExt, EX_rt, EX_rd, EX_PCAddResult,
+				 EX_RegDst, EX_Branch, EX_MemRead, EX_MemtoReg, EX_ALUOp, EX_MemWrite, 
+				 EX_ALUSrc, EX_RegWrite, /*EX_Jump,*/ EX_CondMov
+			   );
 
 input Clk;
-input RegWrite, MemtoReg, MemWrite, MemRead, RegDst, ALUSrc;
-input wire [3:0] ALUOp;
-input wire [31:0] ID_PCAddResult, ReadData1, ReadData2, immExt;
+input wire [31:0] ReadData1, ReadData2, immExt, ID_PCAddResult;
 input wire [4:0] rt, rd;
+input wire [3:0] ALUOp;
+input RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, /*Jump,*/ CondMov; 
 
-output reg EX_RegWrite, EX_MemtoReg, EX_MemWrite, EX_MemRead, EX_RegDst, EX_ALUSrc;
-output reg [3:0] EX_ALUOp;
-output reg [31:0] EX_PCAddResult, EX_ReadData1, EX_ReadData2, EX_immExt;
+output reg [31:0] EX_ReadData1, EX_ReadData2, EX_immExt, EX_PCAddResult;
 output reg [4:0] EX_rt, EX_rd;
-
-
+output reg [3:0] EX_ALUOp;
+output reg EX_RegDst, EX_Branch, EX_MemRead, EX_MemtoReg, EX_MemWrite, EX_ALUSrc, EX_RegWrite, /*EX_Jump,*/ EX_CondMov;
 
 always @(posedge Clk) begin
-	
-	// Controls
-	EX_RegWrite <= RegWrite;
-	EX_MemtoReg <= MemtoReg;
-	EX_MemWrite <= MemWrite;
-	EX_MemRead <= MemRead;
-	EX_RegDst <= RegDst;
-	EX_ALUSrc <= ALUSrc;
-	EX_ALUOp <= ALUOp;
-	
-	// Data
-	EX_PCAddResult <= ID_PCAddResult;
 	EX_ReadData1 <= ReadData1;
 	EX_ReadData2 <= ReadData2;
 	EX_immExt <= immExt;
+	EX_PCAddResult <= ID_PCAddResult;
 	EX_rt <= rt;
 	EX_rd <= rd;
+	EX_ALUOp <= ALUOp;
 
+	EX_RegDst <= RegDst;
+	EX_Branch <= Branch;
+	EX_MemRead <= MemRead;
+	EX_MemtoReg <= MemtoReg;
+	EX_MemWrite <= MemWrite;
+	EX_ALUSrc <= ALUSrc;
+	EX_RegWrite <= RegWrite;
+	//EX_Jump <= Jump;
+	EX_CondMov <= CondMov;
 end
 endmodule
--- MEMWB_reg.v
+++ MEMWB_reg.v
@@ -1,51 +1,27 @@
 `timescale 1ns / 1ps
 
 module MEMWB_reg(
-//		inputs
-//			For internal use:
-				Clk,
-
-//			To Pass through:
-//				Control lines;
-					MEM_RegWrite,
-					MEM_MemtoReg,
-				
-//				Data lines;
-					MEM_DMResult,
-					MEM_ALUResult,
-					MEM_WriteRegister,
-					
-
-//		outputs
-//			To pass through:
-// 				Control lines;
-					WB_RegWrite,
-					WB_MemtoReg,
-				
-//				Data lines;				
-					WB_DMResult,
-					WB_ALUResult,
-					WB_WriteRegister );
+	/* inputs */ Clk, MEM_DMResult, MEM_ALUResult, MEM_WriteRegister,
+				 MEM_MemtoReg, MEM_RegWrite,
+	/* outputs */ WB_DMResult, WB_ALUResult, WB_WriteRegister,   
+				 WB_MemtoReg, WB_RegWrite
+			   );
 
 input Clk;
-input MEM_RegWrite, MEM_MemtoReg; 
 input wire [31:0] MEM_DMResult, MEM_ALUResult;
 input wire [4:0]  MEM_WriteRegister; 
+input MEM_MemtoReg, MEM_RegWrite; 
 
-output reg WB_RegWrite, WB_MemtoReg;
 output reg [31:0] WB_DMResult, WB_ALUResult;
 output reg [4:0]  WB_WriteRegister;
+output reg WB_MemtoReg, WB_RegWrite;
 
 always @(posedge Clk) begin
-	
-	// Control
-	WB_RegWrite <= MEM_RegWrite;
-	WB_MemtoReg <= MEM_MemtoReg;
-	
-	// Data
 	WB_DMResult <= MEM_DMResult;
 	WB_ALUResult <= MEM_ALUResult;
 	WB_WriteRegister <= MEM_WriteRegister;
 
+	WB_MemtoReg <= MEM_MemtoReg;
+	WB_RegWrite <= MEM_RegWrite;
 end
 endmodule
